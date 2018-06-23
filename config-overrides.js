dc4629282c3c4eb8fd58aefdff0aa9a65cac8a63
Jayphen <jayphen@gmail.com>
2018-06-23 18:15:56 +0200

    Adds emotion

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,17 @@
-// const { injectBabelPlugin } = require("react-app-rewired");
 const {
   rewireWebpack: rewireTypescript,
   rewireJest: rewireTypescriptJest,
   rewireTSLint
 } = require("react-app-rewire-typescript-babel-preset");
+const { rewireEmotion } = require("react-app-rewire-emotion");
 
 module.exports = {
   webpack: function(config, env) {
     config = rewireTypescript(config);
     config = rewireTSLint(config /* {} - optional tslint-loader options */);
+    config = rewireEmotion(config, env, { inline: true });
     return config;
   },
-  // babel: function(config, env) {
-  //   return rewireEmotion(config, env, { inline: true });
-  // },
   jest: function(config) {
     return rewireTypescriptJest(config);
   }
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import styled from "react-emotion";
 import "./App.css";
 import Wow from "./Hello";
 // @ts-ignore I dunno why TS doesn't like the logo
 import logo from "./logo.svg";
+
+const Test = styled.div`
+  color: pink;
+  border-bottom: 3px solid rgba(255, 255, 255, 0.2);
+  margin-top: 1rem;
+`;
 
 class App extends Component {
   public render() {
@@ -14,6 +21,7 @@ class App extends Component {
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <Wow msg="Typescript yay" />
+          <Test>also react-emotion</Test>
         </header>
       </div>
     );
